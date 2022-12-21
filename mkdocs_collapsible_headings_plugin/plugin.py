c8e828f21f67d12d2ca0366379121140b67d88a2
hannes <hannesdelbeke@gmail.com>
2022-12-21 11:02:10 +0000

    rename header to heading, MVP toggle headings

--- mkdocs_collapsible_headings_plugin/plugin.py
+++ mkdocs_collapsible_headings_plugin/plugin.py
@@ -1,63 +1,56 @@
-# import os
-# import sys
-# from timeit import default_timer as timer
-# from datetime import datetime, timedelta
-#
+import os
+import sys
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+
 # from mkdocs import utils as mkdocs_utils
 # from mkdocs.config import config_options, Config
 # from mkdocs.plugins import BasePlugin
-BasePlugin = object
-config_options = object
+
+from unittest.mock import MagicMock
+BasePlugin = MagicMock()
+config_options = MagicMock()
 
 import itertools
 from bs4 import BeautifulSoup
 
-# try online https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_header
-s = """
-<article>
-  <header>
-    <h1>HEAD1</h1>
-        <p>Posted by John Doe</p>
-        <p>Some additional information here</p>
-        <h2>HEAD2</h2>
-            <p>Posted by John Doe</p>
-            <p>Some additional information here</p>
-    <h1>HEAD1B</h1>
-  </header>
-  <p>Lorem Ipsum dolor set amet....</p>
-</article>
-"""
 
-def wrap(soup, header_name):
-    """wrap all headers and next siblings into sections"""
+def wrap(soup, heading_name):
+    """wrap all headings and next siblings into sections"""
     # thanks https://stackoverflow.com/a/32290036/7924473
 
-    headers = soup.find_all(header_name)
-    for element in headers:
+    headings = soup.find_all(heading_name)
+    for element in headings:
         els = [i for i in itertools.takewhile(
                   lambda x: x.name not in [element.name, 'script'],
                   element.next_siblings)]
-        new_tag = soup.new_tag('div', class_='collapsible')
-        element.insert_after(new_tag)
+
+        # all classes start with md-ch- to avoid conflicts with other plugins
+        # md : makedocs
+        # ch : collapsible headings
+
+        new_tag_toggle = soup.new_tag('input type="checkbox"', class_='md-ch-heading-toggle')
+        new_tag_div = soup.new_tag('div', class_='md-ch-heading-content')
+
+        element.insert_before(new_tag_toggle)
+        element.insert_after(new_tag_div)
+
         for tag in els:
-            new_tag.append(tag)
+            new_tag_div.append(tag)
 
     return soup
 
 
-
-def make_headers_collapsable(html_string: str) -> str:
+def make_headings_collapsible(html_string: str) -> str:
     soup = BeautifulSoup(html_string, 'html.parser')
-    for header_name in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
-        soup = wrap(soup, header_name)
+    # skip h1 since h1 in markdown is h2 in html
+    # when updating this, also update in javascript
+    for heading_name in [ 'h2', 'h3', 'h4', 'h5', 'h6']:
+        soup = wrap(soup, heading_name)
     return str(soup)
 
 
-r = make_headers_collapsable(s)
-print(r)
-
-
-class CollapsableHeaders(BasePlugin):
+class Collapsibleheadings(BasePlugin):
 
     config_scheme = (
         ('param', config_options.Type(str, default='')),
@@ -109,7 +102,7 @@ class CollapsableHeaders(BasePlugin):
     def on_page_content(self, html, page, config, files):
         """The page_content event is called after the Markdown text is rendered to HTML
         (but before being passed to a template) and can be used to alter the HTML body of the page."""
-        return wrap_text_between_headers_in_collapsible_divs(html)
+        return make_headings_collapsible(html)
 
     # def on_page_context(self, context, page, config, nav):
     #     return context
--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='mkdocs-your-plugin',
+    name='mkdocs-collapsible-headings-plugin',
     version='0.1.0',
-    description='A MkDocs plugin',
-    long_description='',
-    keywords='mkdocs',
-    url='',
-    author='Your Name',
-    author_email='your email',
+    description='make headings content collapsable, like in Obsidian',
+    long_description='wrap headings and next siblings into collapsible sections, with class="collapsible"',
+    keywords=['collapsible', 'collapse', 'headings', 'heading', 'toggle', 'obsidian', 'header'],
+    url='https://github.com/hannesdelbeke/mkdocs-collapsable-headers',
+    author='Hannes Delbeke',
+    # author_email='your email',
     license='MIT',
     python_requires='>=2.7',
-    install_requires=[
-        'mkdocs>=1.0.4'
-    ],
+    install_requires=['mkdocs>=1.0.4'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -30,7 +28,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'mkdocs.plugins': [
-            'your-plugin = mkdocs_your_plugin.plugin:YourPlugin'
+            'collapsible-headings = mkdocs_collapsible_headings_plugin.plugin:Collapsibleheadings'
         ]
     }
 )
